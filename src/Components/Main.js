5107c6a225844c09c76176890fe3b6ecd7539128
BasharNobeh <Basharnobeh2001@gmail.com>
2022-08-29 21:26:50 +0300

    msg

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -11,7 +11,8 @@ class Main extends react.Component {
       
       errorFlag: false,
       quoteFlag: false,
-      mapFlag :false,
+      appear:false,
+      
     };
   }
 
@@ -22,12 +23,11 @@ class Main extends react.Component {
       const myKey = "pk.52ffa2b140346333af8917296b40c4cb";
       const city = e.target.city.value;
       const link = `https://us1.locationiq.com/v1/search?key=${myKey}&q=${city}&format=json`;
-      let Mydata = await axios.get(link);
-      console.log(myKey);
-      console.log(city);
-      console.log(link);
-      console.log(Mydata);
-      this.setState({
+      let Mydata = await axios.get(link);  
+     
+      
+        this.setState({
+          
         name: Mydata.data[0].display_name,
         lat: Mydata.data[0].lat,
         lon: Mydata.data[0].lon,
@@ -35,6 +35,25 @@ class Main extends react.Component {
         quoteFlag: true,
         mapFlag :true,
       });
+      
+
+      const server = `http://localhost:3100/weather?name=${e.target.city.value}&lon=${Mydata.data[3].lon}&lat=${Mydata.data[3].lat}`;
+      let CityData = await axios.get(server);
+    
+      
+      CityData = CityData.data;
+      console.log(CityData);  
+      this.setState({
+          
+        name: CityData[0],
+        lat : CityData[1],
+        lon: CityData[2],
+        appear:true,
+        
+      });
+
+
+
     } catch {
       this.setState({
         errorFlag: true,
@@ -56,13 +75,17 @@ class Main extends react.Component {
             information about The requested Location
           </h3>
         )}
-        <h3 style={{backgroundColor:"Silver", width:"400px" , height:"50px", padding: "30px 0" ,border: "3px solid black" , borderRadius:"20px" , marginLeft:"20px"}}>Name : {this.state.name} <br></br></h3>
 
-        <h3 style={{backgroundColor:"Silver", width:"400px" , height:"50px", padding: "30px 0" ,border: "3px solid black" , borderRadius:"20px" , marginLeft:"20px"}}> Lat : {this.state.lat} </h3>
+<h3 >Name : {this.state.name} <br></br></h3>
 
-        <h3 style={{backgroundColor:"Silver", width:"400px" , height:"50px", padding: "30px 0" ,border: "3px solid black" , borderRadius:"20px" , marginLeft:"20px"}}> Lon : {this.state.lon} </h3>
+<h3 > Lat : {this.state.lat} </h3>
 
-        {this.state.mapFlag && <img style={{backgroundColor:"Silver", width:"700px" , height:"350px", padding: "3px 0" ,border: "3px solid black" , borderRadius:"2px" , marginLeft:"20px"}} src = {`https://maps.locationiq.com/v3/staticmap?key=pk.52ffa2b140346333af8917296b40c4cb&center=${this.state.lat},${this.state.lon}`}/>}
+<h3 > Lon : {this.state.lon} </h3>
+
+         
+    
+
+      
      
         
 
